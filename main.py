d55091a40378197af4a1b07a3c34c710456da03c
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2025-06-11 00:20:26 +0300

    fix

--- main.py
+++ main.py
@@ -2152,7 +2152,7 @@ async def process_item(callback_query: CallbackQuery, state: FSMContext):
     analysis_result1 = await run_with_timeout(
     bot=bot,
     us_id=us_id,
-    coro=lambda: no_thread_ass(str(db_info), GENERAL_ANALYSIS_ASS),
+    coro=no_thread_ass(str(db_info), GENERAL_ANALYSIS_ASS),
     timeout=15,
     timeout_message="Извините что задерживаюсь, результат все еще в обработке"
 )
